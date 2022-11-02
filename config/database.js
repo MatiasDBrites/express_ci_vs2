18c760f117f1d5d38358d87ff130cc7ec5f94764
MatiasDBrites <matias.brites@gmail.com>
2022-11-01 23:54:46 -0500

    bug dialect

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ process.env.DB_USER,
 process.env.DB_PASSWORD,
 {
 host: process.env.DB_HOST,
-dialect: process.env.DB_DIALECT,
+dialect: "postgres",
 logging: false,
 port: process.env.DB_PORT,
 define: {
